07255fc3ab6554730cd5b970217a2df20b2984e5
Jhoan Lozano <jhoanlozano980515@gmail.com>
2022-09-15 01:31:33 -0500

    se agrega el script de la base de datos al repo

---
+++ DB/query_TALLER_CONFIABLE_D2ALE.sql
@@ -0,0 +1,343 @@
+--CREATE DATABASE PRUEBA
+USE PRUEBA
+
+CREATE TABLE rol 
+(
+    idRol INT PRIMARY KEY IDENTITY,
+    nombreRol VARCHAR(20),
+)
+
+create table usuario
+(
+    idUsuario INT PRIMARY KEY IDENTITY,
+    identificacion VARCHAR(20) NOT NULL,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    correo VARCHAR(50) not NULL,
+    contrasena varchar(100) not null,
+    fechaNacimiento DATE,
+    rolFK int FOREIGN KEY(rolFK)
+    REFERENCES Rol(idRol)
+)
+
+CREATE TABLE vehiculo
+(
+    placa varchar(10) PRIMARY KEY NOT NULL,
+    marca VARCHAR(20) NOT NULL,
+    modelo VARCHAR(50) NOT NULL,
+    tipoVehiculo VARCHAR(50) NOT NULL,
+    cilindraje VARCHAR(50) NOT NULL,
+    ciudadRegistro VARCHAR(50) NOT NULL, 
+    usuarioFK int FOREIGN KEY(usuarioFK)
+    REFERENCES usuario(idUsuario)
+)
+
+CREATE TABLE soat 
+(
+    idSOAT int PRIMARY KEY IDENTITY,
+    fechaCompra DATE,
+    fechaVencimiento DATE,
+    vehiculoFK VARCHAR(10) FOREIGN KEY (vehiculoFK)
+    REFERENCES vehiculo(placa)
+
+)
+
+CREATE TABLE repuesto 
+(
+    idRepuesto INT PRIMARY KEY IDENTITY,
+    nombreRepuesto VARCHAR(50) NOT NULL,
+    precio int NOT NULL
+)
+
+CREATE TABLE servicio
+(
+    idServicio INT PRIMARY KEY IDENTITY,
+    tipoServicio VARCHAR(20) NOT NULL ,
+    descripcion VARCHAR(255) NOT NULL,
+    nivAceite VARCHAR(20) NOT NULL,
+    nivLiquidoFrenos VARCHAR(20) NOT NULL,
+    nivRefrigerante VARCHAR(20) NOT NULL,
+    nivLiquidoDireccion VARCHAR(20) NOT NULL,
+    fechaCompra DATETIME,
+    vehiculoFK VARCHAR(10) FOREIGN KEY (vehiculoFK)
+    REFERENCES vehiculo(placa),
+    mecanicoFK INT FOREIGN KEY (mecanicoFK) 
+    REFERENCES usuario(idUsuario)
+)
+
+CREATE table maestro_detalle_rep
+(
+idMaestro int PRIMARY KEY IDENTITY,
+repuestoFK int FOREIGN KEY(repuestoFK)
+references repuesto(idRepuesto),
+servicioFK int FOREIGN KEY(servicioFK)
+REFERENCES servicio(idServicio)
+)
+insert into rol(nombreRol) VALUES ('Cliente');
+insert into rol(nombreRol) VALUES ('Auxiliar');
+insert into rol(nombreRol) VALUES ('Jefe de Operaciones');
+insert into rol(nombreRol) VALUES ('Mecánico');
+GO
+
+create PROCEDURE sp_ListarRol
+as 
+BEGIN
+    select * from rol
+END
+GO
+
+create PROCEDURE sp_ObtenerRol(@idRol int)
+AS
+BEGIN
+    SELECT * from rol where idRol=@idRol 
+END
+
+insert into usuario(identificacion, nombres, apellidos, telefono, correo, contrasena, fechaNacimiento,rolFK) VALUES (1234,'Otro','Usuario','3203019435','jl@gmail.com','1234', '1998-05-15',1);
+insert into usuario(identificacion, nombres, apellidos, telefono, correo, contrasena, fechaNacimiento,rolFK) VALUES (1234,'Otro2','Usuario2','3203019435','jl@gmail.com','1234', '1998-05-15',2);
+insert into usuario(identificacion, nombres, apellidos, telefono, correo, contrasena, fechaNacimiento,rolFK) VALUES (1234,'Otro3','Usuario3','3203019435','jl@gmail.com','1234', '1998-05-15',3);
+insert into usuario(identificacion, nombres, apellidos, telefono, correo, contrasena, fechaNacimiento,rolFK) VALUES (1234,'Otro4','Usuario4','3203019435','jl@gmail.com','1234', '1998-05-15',4);
+
+
+
+GO
+
+CREATE PROCEDURE sp_ListarUsuario
+AS
+BEGIN
+    SELECT idUsuario, identificacion, nombres, apellidos, telefono, correo, contrasena, fechaNacimiento, roles.nombreRol 'rol'
+    FROM usuario usuarios
+    INNER JOIN rol roles
+    ON roles.idRol = usuarios.rolFK
+END
+GO
+
+CREATE PROCEDURE sp_ObtenerUsuario(@idUsuario int)
+AS
+BEGIN
+    SELECT * FROM usuario WHERE idUsuario = @idUsuario
+END
+GO
+
+CREATE PROCEDURE sp_GuardarUsuario(
+    @nombres VARCHAR(50),
+    @apellidos VARCHAR(50),
+    @identificacion VARCHAR(20),
+    @telefono VARCHAR(15),
+    @correo VARCHAR(50),
+    @contrasena varchar(100),
+    @fechaNacimiento DATE,
+    @rolFK int
+)
+AS
+BEGIN
+    INSERT INTO usuario(nombres, apellidos, identificacion, telefono, correo, contrasena, fechaNacimiento, rolFK) VALUES (@nombres, @apellidos,@identificacion,@telefono,@correo,@contrasena,@fechaNacimiento,@rolFK)
+END
+GO
+
+CREATE PROCEDURE sp_EditarUsuario(
+    @idUsuario int,
+    @nombres VARCHAR(50),
+    @apellidos VARCHAR(50),
+    @identificacion VARCHAR(20),
+    @telefono VARCHAR(15),
+    @correo VARCHAR(50),
+    @contrasena varchar(100),
+    @fechaNacimiento DATE,
+    @rolFK int
+)
+AS
+BEGIN
+    UPDATE usuario SET nombres = @nombres, apellidos = @apellidos, identificacion = @identificacion, telefono = @telefono, correo = @correo, contrasena = @contrasena, fechaNacimiento = @fechaNacimiento, rolFK = @rolFK WHERE idUsuario = @idUsuario
+END
+GO
+
+CREATE PROCEDURE sp_EliminarUsuario(@idUsuario INT)
+AS
+BEGIN
+    DELETE FROM usuario WHERE idUsuario = @idUsuario
+END
+
+GO
+CREATE PROCEDURE sp_ListarServicio AS
+BEGIN
+    SELECT * FROM servicio
+END
+
+GO
+
+CREATE PROCEDURE sp_ObtenerServicio (@idServicio int) AS
+BEGIN
+    SELECT * FROM servicio WHERE idServicio = @idServicio
+END
+
+GO
+
+CREATE PROCEDURE sp_GuardarServicio 
+(
+    @tipoServicio VARCHAR (20) ,
+    @descripcion VARCHAR(255),
+    @nivAceite VARCHAR (20),
+    @nivLiquidoFrenos VARCHAR(20),
+    @nivLiquidoDireccion VARCHAR(20),
+    @nivRefrigerante VARCHAR(20),
+    @fechaCompra DATETIME,
+    @vehiculoFK VARCHAR(50),
+    @mecanicoFK INT
+) AS
+BEGIN
+    INSERT INTO servicio 
+    (
+        tipoServicio, descripcion, nivAceite, nivLiquidoFrenos, 
+        nivLiquidoDireccion, nivRefrigerante, fechaCompra, vehiculoFK, mecanicoFK
+    ) 
+    VALUES 
+    (
+        @tipoServicio , @descripcion , @nivAceite, @nivLiquidoFrenos,
+        @nivLiquidoDireccion, @nivRefrigerante, @fechaCompra , @vehiculoFK,
+        @mecanicoFK
+    )
+END
+
+GO
+
+CREATE PROCEDURE sp_EditarServicio
+(
+    @idServicio int,
+    @tipoServicio VARCHAR (20) ,
+    @descripcion VARCHAR(255),
+    @nivAceite VARCHAR (20),
+    @nivLiquidoFrenos VARCHAR(20),
+    @nivLiquidoDireccion VARCHAR(20),
+    @nivRefrigerante VARCHAR(20),
+    @fechaCompra DATETIME,
+    @vehiculoFK VARCHAR(50),
+    @mecanicoFK INT
+) AS
+BEGIN
+    UPDATE servicio SET
+        tipoServicio = @tipoServicio, descripcion = @descripcion, 
+        nivAceite = @nivAceite, nivLiquidoFrenos = @nivLiquidoFrenos, 
+        nivLiquidoDireccion = @nivLiquidoDireccion, nivRefrigerante = @nivRefrigerante, 
+        fechaCompra = @fechaCompra , vehiculoFK = @vehiculoFK, mecanicoFK = @mecanicoFK
+    WHERE idServicio = @idServicio
+END
+GO
+
+CREATE PROCEDURE sp_EliminarServicio (@idServicio int) AS
+BEGIN
+    DELETE FROM servicio WHERE idServicio = @idServicio
+END
+
+GO
+CREATE procedure sp_GuardarSoat(
+@vehiculoFK VARCHAR(10),
+@fechaCompra DATE,
+@fechaVencimiento DATE
+)
+as
+begin
+         insert into soat(vehiculoFK,fechaCompra,fechaVencimiento) values(@vehiculoFK,@fechaCompra,@fechaVencimiento)
+end
+GO
+
+CREATE procedure sp_ListarSoat
+as
+BEGIN
+     select * from soat
+END
+GO
+
+CREATE procedure sp_EditarSoat(
+@idSOAT int,
+@fechaCompra DATE,
+@fechaVencimiento DATE
+)
+as
+begin
+         update soat set  fechaCompra=@fechaCompra,
+          fechaVencimiento=@fechaVencimiento  where idSOAT=@idSOAT
+end
+GO
+
+CREATE PROCEDURE sp_EliminarSoat(
+    @idSOAT INT
+)
+as 
+BEGIN
+    delete from soat where idSOAT=@idSOAT
+end
+
+--Stored Procedures Vehiculo--
+GO
+CREATE PROCEDURE sp_ListarVehiculo
+AS
+BEGIN
+    SELECT placa, marca, modelo, tipoVehiculo, cilindraje, ciudadRegistro, usuarios.nombres 'nombre',usuarios.apellidos 'apellido'
+    FROM vehiculo vehiculos
+    INNER JOIN usuario usuarios
+    ON usuarios.idUsuario = vehiculos.usuarioFK
+END
+
+GO
+CREATE PROCEDURE sp_ObtenerVehiculo(@placa varchar(10))
+AS
+BEGIN
+    --SELECT * FROM vehiculo WHERE placa = @placa
+
+    SELECT placa, marca, modelo, tipoVehiculo, cilindraje, ciudadRegistro, usuario.identificacion
+    FROM vehiculo
+    INNER JOIN usuario
+    ON usuario.idUsuario = vehiculo.usuarioFK WHERE vehiculo.placa = @placa
+END
+
+GO
+CREATE PROCEDURE sp_GuardarVehiculo(
+@placa VARCHAR(10),
+@marca VARCHAR(20),
+@modelo VARCHAR(50),
+@tipoVehiculo VARCHAR(50),
+@cilindraje VARCHAR(50),
+@ciudadRegistro VARCHAR(50),
+@identificacion VARCHAR(20)
+)
+AS
+BEGIN
+    DECLARE @usuarioFK int = 
+    (SELECT usuario.idUsuario FROM usuario WHERE usuario.identificacion = @identificacion)
+
+    INSERT INTO vehiculo(placa, marca, modelo, tipoVehiculo, cilindraje, ciudadRegistro, usuarioFK)VALUES(@placa, @marca, @modelo, @tipoVehiculo, @cilindraje, @ciudadRegistro, @usuarioFK)
+END
+
+GO
+CREATE PROCEDURE sp_EditarVehiculo(
+    @placa VARCHAR(10),
+    @marca VARCHAR(20),
+    @modelo VARCHAR(50),
+    @tipoVehiculo VARCHAR(50),
+    @cilindraje VARCHAR(50),
+    @ciudadRegistro VARCHAR(50),
+    @identificacion VARCHAR(20)
+)
+AS
+BEGIN
+    DECLARE @usuarioFK int = (SELECT usuario.idUsuario FROM usuario WHERE usuario.identificacion = @identificacion)
+
+    UPDATE vehiculo SET marca = @marca, modelo = @modelo, tipoVehiculo = @tipoVehiculo, cilindraje = @cilindraje, ciudadRegistro = @ciudadRegistro, usuarioFK = @usuarioFK WHERE placa = @placa
+END
+
+--IMPORTANTE
+--¿Creamos procedimiento para editar la placa?
+
+GO
+CREATE PROCEDURE sp_EliminarVehiculo(@placa VARCHAR(10))
+AS
+BEGIN
+    DELETE FROM vehiculo WHERE placa = @placa
+END
+
+execute sp_ListarVehiculo;
+EXECUTE sp_ListarUsuario
+EXECUTE sp_ObtenerVehiculo(ABC-123)
+
+--drop PROCEDURE sp_ObtenerVehiculo
