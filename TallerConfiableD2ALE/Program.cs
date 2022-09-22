48991c9cba02d3a0a92219cfa2b04f258ab4a12e
Edwar Garcia <garciapatinoedwar26@gmail.com>
2022-09-21 23:29:24 -0500

    modificacion a la vista index

--- TallerConfiableD2ALE/Program.cs
+++ TallerConfiableD2ALE/Program.cs
@@ -44,6 +44,6 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=MantenedorUsuario}/{action=Listar}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
